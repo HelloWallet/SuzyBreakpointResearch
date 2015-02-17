6a5d0b517852650badfb49dfde1e3922ff10b42a
Johnny Reading <johnny@hellowallet.com>
2015-02-17 15:37:29 -0500

    round 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"),
-	sass = require("gulp-sass"),
+	//sass = require("gulp-sass"),
+	sass = require('gulp-ruby-sass'),
 	connect = require("gulp-connect");
 
 var handleErrors = function () {
@@ -7,28 +8,36 @@ var handleErrors = function () {
 };
 
 gulp.task("sass", function() {
-	return gulp.src("src/styles/*.scss")
-		.pipe(sass())
+	return sass("src/styles/app.scss",
+		{
+			require: ["susy", "breakpoint"],
+			style: "normal"
+		})
 		.on("error", handleErrors)
-		.pipe(gulp.dest("./app/styles/"));
+		.pipe(gulp.dest("app/styles"));
 });
 
-gulp.task("copy", function() {
+gulp.task("copy-html", function() {
 	return gulp.src("src/html/*.html")
   		.pipe(gulp.dest("./app"));
 });
 
+gulp.task("copy-css", function() {
+	return gulp.src("bower_components/**/*.css")
+  		.pipe(gulp.dest("./app/styles"));
+});
+
 gulp.task("server", function() {
-  connect.server({
-    livereload: true,
-    root: ["./app"],
-    port: 1337
-  });
+	connect.server({
+		//livereload: true,
+		root: ["./app"],
+		port: 1337
+	});
 });
 
 gulp.task("watch", function() {
-    gulp.watch("src/styles/*.scss", ["sass"]);
-    gulp.watch("src/html/*.html", ["copy"]);
+	gulp.watch("src/styles/*.scss", ["sass"]);
+	gulp.watch("src/html/*.html", ["copy-html"]);
 });
 
-gulp.task("default", ["sass", "copy", "server", "watch"]);
+gulp.task("default", ["sass", "copy-html", "copy-css", "server", "watch"]);
